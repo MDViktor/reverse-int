2c7c26f6d7063cac715cba18b3b12a1f5627e9e6
MDViktor <milliondollarboymvd@gmail.com>
2022-07-06 19:57:50 +0300

    init: reverseIntFunction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const x = String(Math.abs(n))
+  let i = 0;
+  result = '';
+  while (i < x.length) {
+    result = `${x[i]}${result}`;
+    i = i + 1;
+  }
+  return Number(result);
 }
